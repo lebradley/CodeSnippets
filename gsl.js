d0bb1476c127e6b9cdd12cb4cf224d2bbe658685
lbradley <lbradley@and.digital>
2018-04-05 09:23:58 +0100

    end of wednesday

--- gsl.js
+++ gsl.js
@@ -36,9 +36,7 @@ function processSearchValue(targetdiv) {
     } else {
       let place = results[0].geometry.location;
       let location = { lat: place.lat, lng: place.lng };
-      console.log(location);
       getGSLByLatLong(location, respObject => {
-        console.log(respObject);
         //reposition map to new locale
         let center = new google.maps.LatLng(location.lat, location.lng);
         map.panTo(center);
@@ -49,7 +47,21 @@ function processSearchValue(targetdiv) {
               lat: parseFloat(s.latitude),
               lng: parseFloat(s.longitude)
             },
-            map: map
+            map: map,
+            label: s.storeFacilities.find(f => {
+              return f.name === 'Costa Express' && f.active == true;
+              console.log(f);
+            })
+              ? 'E'
+              : null
+          });
+          const infoWindow = new google.maps.InfoWindow({
+            content: s.storeAddress.addressLine1
+          });
+          // improvement - when another window is clicked it becomes the focus and this closes down...
+          marker.addListener('click', () => {
+            infoWindow.close();
+            infoWindow.open(map, marker);
           });
         });
       });
@@ -74,43 +86,27 @@ function getGSLByLatLong(coords, callback) {
   xmlHttp.send(null);
 }
 
-// function processSearchValue2(targetdiv) {
-//   let value = document.getElementById(targetdiv).value;
-//   let latLong = getStoreByPostCode(baseGSLUrl, value, responseText => {
-//     const latLong = createLatLongObj(responseText);
-//     let location = { lat: latLong.lat, lng: latLong.long };
-//     map = new google.maps.Map(document.getElementById('map'), {
-//       zoom: 12,
-//       center: location
-//     });
-//     let marker = new google.maps.Marker({
-//       position: location,
-//       map: map
-//     });
-//   });
+// function createLatLongObj(responseText) {
+//   const long = responseText.stores[0].longitude;
+//   const lat = responseText.stores[0].latitude;
+//   const latLong = {
+//     long: parseFloat(long),
+//     lat: parseFloat(lat)
+//   };
+//   return latLong;
 // }
 
-function createLatLongObj(responseText) {
-  const long = responseText.stores[0].longitude;
-  const lat = responseText.stores[0].latitude;
-  const latLong = {
-    long: parseFloat(long),
-    lat: parseFloat(lat)
-  };
-  return latLong;
-}
-
-function getStoreByPostCode(theUrl, postalCode, callback) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      let respObj = JSON.parse(xmlHttp.responseText);
-      callback(respObj);
-    }
-  };
-  xmlHttp.open('GET', theUrl + 'postalCode=' + postalCode, true); // true marks asynchronous
-  xmlHttp.send(null);
-}
+// function getStoreByPostCode(theUrl, postalCode, callback) {
+//   var xmlHttp = new XMLHttpRequest();
+//   xmlHttp.onreadystatechange = function() {
+//     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+//       let respObj = JSON.parse(xmlHttp.responseText);
+//       callback(respObj);
+//     }
+//   };
+//   xmlHttp.open('GET', theUrl + 'postalCode=' + postalCode, true); // true marks asynchronous
+//   xmlHttp.send(null);
+// }
 
 // Starter method to layout the map when the user first lands on the page. Fix the HTML 5 Geolocation aspect
 function initMap() {
@@ -141,9 +137,5 @@ function makeAndPlotMap(coords) {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: coords
-  });
-  let marker = new google.maps.Marker({
-    position: coords,
-    map: map
   });
 }
